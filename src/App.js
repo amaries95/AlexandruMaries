dc03a275ee73dde66bc447f3cec0cdede9d7161f
Alex <alexandru.maries@plainconcepts.com>
2022-06-20 15:01:51 +0300

    working on footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { Col, Row } from 'react-bootstrap';
-import {
-  BrowserRouter as Router, Route, Routes
-} from "react-router-dom";
+import { Col, Container, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Education from './components/Education/Education';
 import Experience from './components/Experience/Experience';
@@ -12,33 +10,42 @@ import Projects from './components/Projects/Projects';
 import Recommendation from './components/Recommendation/Recommendation';
 import Skills from './components/Skills/Skills';
 
-
 function App() {
   return (
     <Router>
-      <Row className="vh-100 gx-0">
-        <Col xs={3} className="col-divider">
-          <HeaderMenu />
-        </Col>
-        <Col>
+      <Container fluid={true} className="container-padding">
+        <Row className="vh-100 gx-0">
+          <Col xs={3} className="col-divider">
+            <HeaderMenu />
+          </Col>
+          <Col>
             <Routes>
-              <Route path='/AlexandruMaries/' element={<Home />}>
-              </Route>
-              <Route path='/AlexandruMaries/experience' element={<Experience/>}>
-              </Route>
-              <Route path='/AlexandruMaries/education' element={<Education></Education>}>
-              </Route>
-              <Route path='/AlexandruMaries/projects' element={<Projects></Projects>}>
-              </Route>
-              <Route path='/AlexandruMaries/skills' element={<Skills></Skills>}>
-              </Route>
-              <Route path='/AlexandruMaries/recommendation' element={<Recommendation></Recommendation>}>
-              </Route>
+              <Route path="/AlexandruMaries/" element={<Home />}></Route>
+              <Route
+                path="/AlexandruMaries/experience"
+                element={<Experience />}
+              ></Route>
+              <Route
+                path="/AlexandruMaries/education"
+                element={<Education></Education>}
+              ></Route>
+              <Route
+                path="/AlexandruMaries/projects"
+                element={<Projects></Projects>}
+              ></Route>
+              <Route
+                path="/AlexandruMaries/skills"
+                element={<Skills></Skills>}
+              ></Route>
+              <Route
+                path="/AlexandruMaries/recommendation"
+                element={<Recommendation></Recommendation>}
+              ></Route>
             </Routes>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Container>
     </Router>
-
   );
 }
 
--- src/components/HeaderMenu/Footer/Footer.js
+++ src/components/HeaderMenu/Footer/Footer.js
@@ -1,42 +1,50 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { Card, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { LogoFacebook, LogoGithub, LogoLinkedin } from "react-ionicons";
 import style from './Footer.module.css';
 
 export default function Footer(props) {
   return (
-    <Card className={style['bottom-info']}>
-          <Row className={style['social-links']}>
-            <ul>
-              <li>
-                <a href='https://www.linkedin.com/in/alexandrumaries' target='_blank' rel="noreferrer">
-                  <LogoLinkedin 
-                    color={"white"}
-                    width={'40px'}
-                    height={'40px'}>
-                  </LogoLinkedin>
-                </a>
-              </li>
-              <li className={style['list-item']}>
-                <a href='https://github.com/amaries95' target='_blank' rel="noreferrer">
-                  <LogoGithub
-                    color={"white"}
-                    width={'40px'}
-                    height={'40px'}>
-                  </LogoGithub>
-                </a>
-              </li>
-              <li className={style['list-item']}>
-                <a href='https://www.facebook.com/alexandru.maries28' target='_blank' rel="noreferrer">
-                  <LogoFacebook
-                    color={"white"}
-                    width={'40px'}
-                    height={'40px'}>
-                  </LogoFacebook>
-                </a>
-              </li>
-            </ul>
-          </Row>
-    </Card>
+    <Container className={style['bottom-info']}>
+      <Row className="h-25">
+        <Col>
+          <a
+              href="https://www.linkedin.com/in/alexandrumaries"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LogoLinkedin
+                color={'white'}
+                width={'40px'}
+                height={'40px'}
+              ></LogoLinkedin>
+            </a>
+        </Col>
+        <Col>
+          <a
+              href="https://github.com/amaries95"
+              target="_blank"
+              rel="noreferrer">
+              <LogoGithub
+                color={'white'}
+                width={'40px'}
+                height={'40px'}
+              ></LogoGithub>
+            </a>
+        </Col>
+        <Col>
+        <a
+          href="https://www.facebook.com/alexandru.maries28"
+          target="_blank"
+          rel="noreferrer">
+          <LogoFacebook
+            color={'white'}
+            width={'40px'}
+            height={'40px'}
+          ></LogoFacebook>
+        </a>
+        </Col>
+      </Row>
+    </Container>
   );
 }
